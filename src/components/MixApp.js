792d1ebb89dd43a8c01f9659ceec989afc7f3194
dwil2444 <daneandrew16@gmail.com>
2019-03-07 01:49:38 -0500

    added bundle script

--- src/components/MixApp.js
+++ src/components/MixApp.js
@@ -6,4 +6,12 @@ export default class MixApp extends React.Component
   constructor(props) {
     super(props);
   }
+  render () {
+    return (
+      <div>
+         <p>Hello World</p>
+      </div>
+    ); 
+  }
+
 }
